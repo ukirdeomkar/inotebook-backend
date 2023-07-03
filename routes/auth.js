bd41a98f95fd697b3dba63e2975fb9cd10a4d2f3
ukirdeomkar <ukirdeomkar4@gmail.com>
2023-07-03 17:58:40 +0530

    used express-validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../modules/User');
-router.post('/', (req , res )=>{
+const { body, validationResult } = require('express-validator');
+
+router.post('/', [
+ body('name',"The name should be atleast 3 characters").isLength({min : 3}),
+body('email',"Email is invalid").isEmail(),
+body('password',"Password should be atleast 5 characters").isLength({min:5}),   
+]
+
+,(req , res )=>{
     console.log(req.body)
-    const user = User(req.body);
-    user.save();
-    res.json(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    
+    User.create({
+        name : req.body.name,
+        password : req.body.password,
+        email : req.body.email
+    }).then(user => res.json(user))
+    .catch(
+        err=>{
+        console.log(err)
+        res.json({error : "Account with this email already exist" , message : err.message})
+        }
+    );
+    // const user = User(req.body);
+    // user.save();
+    // res.json(req.body);
 
 })
 
